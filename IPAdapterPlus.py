572bf8f04792094d500df9d8c99fbe260612ab08
11dogzi <695660735@qq.com>
2024-06-25 22:15:20 +0800

    AdapterMadAssistant

--- IPAdapterPlus.py
+++ IPAdapterPlus.py
@@ -2,6 +2,7 @@ import torch
 import os
 import math
 import folder_paths
+import random
 
 import comfy.model_management as model_management
 from node_helpers import conditioning_set_values
@@ -1743,6 +1744,57 @@ class IPAdapterCombineParams:
 
         return (ipadapter_params, )
 
+class IPAdapter_Mad_Assistant:
+    CATEGORY_KEYS = ['0layer', '1layer', '2layer', '3layer', '4layer', '5layer', '6layer', '7layer', '8layer', '9layer', '10layer']
+    OPTIONS = {str(i): str(i) for i in range(0, 11)}
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                **cls.get_input_types_from_keys(cls.CATEGORY_KEYS),
+                "Random": (["Yes", "No"], {"default": "No"}),
+                "seed": ("INT", {"default": 0, "min": -1125899906842624, "max": 1125899906842624}),
+            }
+        }
+
+    @staticmethod
+    def get_input_types_from_keys(keys):
+        input_types = {}
+        for i, key in enumerate(keys):
+            # Hide the super key by not including it in the input_types dictionary
+            input_types[f"{key} Weight"] = ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.1, "display": "slider"})
+        return input_types
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("layer_weights",)
+    FUNCTION = "generate_prompt"
+    CATEGORY = "ipadapter"
+
+    def generate_prompt(self, **kwargs):
+        prompt_parts = {str(i): 0.0 for i in range(0, 11)}
+        for i, key in enumerate(self.CATEGORY_KEYS):
+            # Since the super key is hidden, we use the index to find the corresponding weight
+            weight_key = f"{key} Weight"
+            if weight_key in kwargs and kwargs[weight_key] is not None:
+                weight = kwargs[weight_key]
+                prompt_parts[str(i)] = weight
+        
+        if kwargs.get("Random") == "Yes":
+            for key in self.CATEGORY_KEYS:
+                options = list(self.OPTIONS.keys())
+                random_choice = random.choice(options)
+                weight_key = f"{key} Weight"
+                if prompt_parts[str(int(random_choice))] == 0.0:
+                    weight = random.uniform(0.0, 1.0)
+                    prompt_parts[random_choice] = weight
+
+        layer_weights = ','.join(f"{k}:{int(v)}" if v in {0.0, 1.0} else f"{k}:{v:.1f}" for k, v in prompt_parts.items())
+        return (layer_weights,)
+
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  Register
@@ -1784,7 +1836,9 @@ NODE_CLASS_MAPPINGS = {
     "IPAdapterPromptScheduleFromWeightsStrategy": IPAdapterPromptScheduleFromWeightsStrategy,
     "IPAdapterRegionalConditioning": IPAdapterRegionalConditioning,
     "IPAdapterCombineParams": IPAdapterCombineParams,
+    "IPAdapter_Mad_Assistant": IPAdapter_Mad_Assistant,
 }
+
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     # Main Apply Nodes
@@ -1822,4 +1876,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "IPAdapterCombineWeights": "IPAdapter Combine Weights",
     "IPAdapterRegionalConditioning": "IPAdapter Regional Conditioning",
     "IPAdapterCombineParams": "IPAdapter Combine Params",
+    "IPAdapter_Mad_Assistant": "IPAdapter Mad Assistant",
 }
